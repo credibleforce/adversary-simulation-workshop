5d354afadcb22e6eab435b65e6f48da9a8daaf0e
scalatar <jamiefrasermcmurray@gmail.com>
2021-06-11 02:27:53 -0700

    Add three stage example

---
+++ stage_rdg_file.ps1
@@ -0,0 +1,17 @@
+invoke-command -ComputerName win19-svr1.lab.lan -UseSSL -Authentication Negotiate -ScriptBlock {
+    iwr -UseBasicParsing "https://github.com/mobia-security-services/adversarysimulationworkshop/raw/main/RDCMan.zip" -OutFile $env:TEMP\RDCMan.zip
+    Expand-Archive -Path $env:TEMP\RDCMan.zip -DestinationPath $env:TEMP -Force
+    cd $env:TEMP\RDCMan
+    Import-Module .\RDCMan.psd1
+    Remove-Item ${env:USERPROFILE}\Desktop\RemoteDesktop.rdg -ErrorAction SilentlyContinue
+    New-RDCManFile -FilePath ${env:USERPROFILE}\Desktop\RemoteDesktop.rdg -Name RDCMan
+    New-RDCManGroup -FilePath ${env:USERPROFILE}\Desktop\RemoteDesktop.rdg -Name RDCMan
+    $Username = 'LAB\a-jspringfield'
+    $Password = 'PASSWORD'
+    $pass = ConvertTo-SecureString -AsPlainText $Password -Force
+    $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+    New-RDCManServer -FilePath ${env:USERPROFILE}\Desktop\RemoteDesktop.rdg -DisplayName RDCMan -Server 'win19-dc1.lab.lan' -Group RDCMan -Credential $Cred
+    Remove-Module $PSScriptRoot\bin\RDCMan.dll -Force -ErrorAction SilentlyContinue
+    Remove-Item -Recurse -Path $env:TEMP\RDCMan -Force -ErrorAction SilentlyContinue
+    Remove-Item -Recurse -Path $env:TEMP\RDCMan.zip -Force -ErrorAction SilentlyContinue
+}
